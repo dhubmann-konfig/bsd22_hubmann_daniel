4e9e8a65083635b095c543fe2ccb27c03324d0ba
Daniel Hubmann <01130491@edu.campus02.at>
2023-06-01 10:36:43 +0200

    added Log4j2 and logger object

--- src/main/java/at/campus02/bsd/Main.java
+++ src/main/java/at/campus02/bsd/Main.java
@@ -2,21 +2,32 @@
  * Main
  * Represents main entry point of application
  * Author: Daniel Hubmann
- * Last Change: 11.05.2023
+ * Last Change: 01.06.2023
  */
 
 package at.campus02.bsd;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Main {
 
 	public static void main(String[] args) {
 
+		logger.info("info logger");
+		
 		Calculator c = new Calculator();
 		c.add(1, 2);
 		c.minus(1, 2);
 		c.divide(1, 2);
 		c.multiply(1, 2);
+		
+		logger.error("error logger");
 
+		
 	}
+	
+	private static Logger logger = LogManager.getLogger();
+
 
 }
