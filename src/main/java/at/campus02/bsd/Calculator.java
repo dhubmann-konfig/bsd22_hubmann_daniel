b79e4a72a40b2e7e5ae5bdc2b88077cd3e4daf99
Daniel Hubmann <01130491@edu.campus02.at>
2023-06-15 10:52:29 +0200

    Add Javadoc to Calculator class

--- src/main/java/at/campus02/bsd/Calculator.java
+++ src/main/java/at/campus02/bsd/Calculator.java
@@ -2,7 +2,7 @@
  * Calculator
  * Represents a calculator
  * Author: Daniel Hubmann
- * Last Change: 01.06.2023
+ * Last Change: 15.06.2023
  */
 
 package at.campus02.bsd;
@@ -10,20 +10,55 @@ package at.campus02.bsd;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+/**
+ * The Calculator class represents a basic calculator.
+ * 
+ * It supports the basic arithmetic operations addition, subtraction,
+ * multiplication and division, and the factorial operation.
+ * 
+ * @author Daniel Hubmann
+ *
+ */
+
 public class Calculator {
 
 	private static Logger logger = LogManager.getLogger();
 
+	/**
+	 * Adds two numbers and returns the result
+	 * 
+	 * @author Daniel Hubmann
+	 * @param number1 the first number
+	 * @param number2 the second number
+	 * @return the sum of number1 and number2
+	 */
 	public double add(double number1, double number2) {
 		logger.debug("add debug log", number1, number2);
 		return number1 + number2;
 	}
 
+	/**
+	 * Subtracts the second number from the first number and returns the result
+	 * 
+	 * @author Daniel Hubmann
+	 * @param number1 the first number
+	 * @param number2 the second number
+	 * @return the difference between number1 and number2
+	 */
 	public double minus(double number1, double number2) {
 		logger.debug("minus debug log", number1, number2);
 		return number1 - number2;
 	}
 
+	/**
+	 * Divides the first number by the second number and returns the result
+	 * 
+	 * @author Daniel Hubmann
+	 * @param number1 the first number
+	 * @param number2 the second number
+	 * @return the result of number1 divided by number2
+	 * @throws ArithmeticException if number2 is equal to 0
+	 */
 	public double divide(double number1, double number2) throws ArithmeticException {
 		logger.debug("divide debug log", number1, number2);
 
@@ -34,11 +69,26 @@ public class Calculator {
 		return number1 / number2;
 	}
 
+	/**
+	 * Multiplies two numbers and returns the result
+	 * 
+	 * @author Daniel Hubmann
+	 * @param number1 the first number
+	 * @param number2 the second number
+	 * @return the product of number1 and nubmer2
+	 */
 	public double multiply(double number1, double number2) {
 		logger.debug("multiply debug log", number1, number2);
 		return number1 * number2;
 	}
 
+	/**
+	 * Calculates the factorial of a given number and returns the result.
+	 * Returns 0 if the number is less than 0
+	 * 
+	 * @param number number to calculate the according factorial
+	 * @return the factorial of number
+	 */
 	public long factorial(int number) {
 		logger.debug("factorial debug log", number);
 
